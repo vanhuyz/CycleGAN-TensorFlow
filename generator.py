63148c1fb7e972a3f7d981e58805ce68f9dceefc
Van Huy <vanphu.quanghuy@glpgs.com>
2017-04-24 12:09:56 +0900

    Change image_size == 128 to image_size <= 128

--- generator.py
+++ generator.py
@@ -26,7 +26,7 @@ class Generator:
       d128 = ops.dk(d64, 128, is_training=self.is_training, norm=self.norm,
           reuse=self.reuse, name='d128')                                # (?, w/4, h/4, 128)
 
-      if self.image_size == 128:
+      if self.image_size <= 128:
         # use 6 residual blocks for 128x128 images
         res_output = ops.n_res_blocks(d128, reuse=self.reuse, n=6)      # (?, w/4, h/4, 128)
       else:

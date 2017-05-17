ddb84799153ef5fa535fe0d01c1c073627a9eee9
Van Huy <vanphu.quanghuy@glpgs.com>
2017-05-17 19:47:18 +0900

    Add use_patchgan option

--- discriminator.py
+++ discriminator.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import ops
 
 class Discriminator:
-  def __init__(self, name, is_training, norm='instance', patch_size=70, use_sigmoid=False):
+  def __init__(self, name, is_training, norm='instance', use_patchgan=False, patch_size=70, use_sigmoid=False):
     self.name = name
     self.is_training = is_training
     self.norm = norm
     self.reuse = False
+    self.use_patchgan = use_patchgan
     self.patch_size = patch_size
     self.use_sigmoid = use_sigmoid
 
@@ -19,24 +20,25 @@ class Discriminator:
               filled with 0.9 if real, 0.0 if fake
     """
     with tf.variable_scope(self.name):
-      batch_size = input.get_shape().as_list()[0]
-      patch = tf.random_crop(input,
-          [batch_size, self.patch_size, self.patch_size, 3])    # (?, 70, 70, 70)
+      if self.use_patchgan:
+        batch_size = input.get_shape().as_list()[0]
+        input = tf.random_crop(input,
+            [batch_size, self.patch_size, self.patch_size, 3])  # (?, 70, 70, 3)
 
       # convolution layers
-      C64 = ops.Ck(patch, 64, reuse=self.reuse, norm=None,
-          is_training=self.is_training, name='C64')             # (?, 35, 35, 64)
+      C64 = ops.Ck(input, 64, reuse=self.reuse, norm=None,
+          is_training=self.is_training, name='C64')             # (?, w/2, h/2, 64)
       C128 = ops.Ck(C64, 128, reuse=self.reuse, norm=self.norm,
-          is_training=self.is_training, name='C128')            # (?, 18, 18, 128)
+          is_training=self.is_training, name='C128')            # (?, w/4, h/4, 128)
       C256 = ops.Ck(C128, 256, reuse=self.reuse, norm=self.norm,
-          is_training=self.is_training, name='C256')            # (?, 9, 9, 256)
+          is_training=self.is_training, name='C256')            # (?, w/8, h/8, 256)
       C512 = ops.Ck(C256, 512,reuse=self.reuse, norm=self.norm,
-          is_training=self.is_training, name='C512')            # (?, 5, 5, 512)
+          is_training=self.is_training, name='C512')            # (?, w/16, h/16, 512)
 
       # apply a convolution to produce a 1 dimensional output (1 channel?)
       # use_sigmoid = False if use_lsgan = True
       output = ops.last_conv(C512, reuse=self.reuse,
-          use_sigmoid=self.use_sigmoid, name='output')          # (?, 5, 5, 1)
+          use_sigmoid=self.use_sigmoid, name='output')          # (?, w/16, h/16, 1)
 
     self.reuse = True
     self.variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self.name)

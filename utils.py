6123fdb5383aad3f7426f228f5cf1d2b01924148
Van Huy <van-huy@cookpad.com>
2018-02-15 20:32:11 +0900

    Check behavior of convert_image_dtype

--- utils.py
+++ utils.py
@@ -10,7 +10,8 @@ def convert2float(image):
   """ Transfrom from int image ([0,255]) to float tensor ([-1.,1.])
   """
   image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-  return (image/127.5) - 1.0
+  # return (image/127.5) - 1.0
+  return image * 2.0 - 1.0
 
 def batch_convert2int(images):
   """

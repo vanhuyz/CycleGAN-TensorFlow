b37966d0a902007bf7eaea4ebe9e3e80e223700e
Radostin Stoyanov <rstoyanov1@gmail.com>
2018-10-07 08:51:58 +0100

    test-reader: Use valid scope name
    Signed-off-by: Radostin Stoyanov <rstoyanov1@gmail.com>

--- reader.py
+++ reader.py
@@ -45,7 +45,7 @@ class Reader():
                                             capacity=self.min_queue_examples + 3*self.batch_size,
                                             min_after_dequeue=self.min_queue_examples)
 
-            tf.summary.image('_input', images)
+            tf.summary.image('input', images)
         return images
 
     def _preprocess(self, image):

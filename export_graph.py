2f16b7d724fae60ff75593ada5616fad0a029e16
Padmaja Bhagwat <padmaja.Bhagwat@inmobi.com>
2021-07-24 14:43:19 +0530

    making the code tensorflow v2.5 compatible

--- export_graph.py
+++ export_graph.py
@@ -24,37 +24,40 @@ tf.flags.DEFINE_integer('ngf', 64,
 tf.flags.DEFINE_string('norm', 'instance',
                        '[instance, batch] use instance norm or batch norm, default: instance')
 
+
 def export_graph(model_name, XtoY=True):
-  graph = tf.Graph()
+    graph = tf.Graph()
 
-  with graph.as_default():
-    cycle_gan = CycleGAN(ngf=FLAGS.ngf, norm=FLAGS.norm, image_size=FLAGS.image_size)
+    with graph.as_default():
+        cycle_gan = CycleGAN(ngf=FLAGS.ngf, norm=FLAGS.norm, image_size=FLAGS.image_size)
 
-    input_image = tf.placeholder(tf.float32, shape=[FLAGS.image_size, FLAGS.image_size, 3], name='input_image')
-    cycle_gan.model()
-    if XtoY:
-      output_image = cycle_gan.G.sample(tf.expand_dims(input_image, 0))
-    else:
-      output_image = cycle_gan.F.sample(tf.expand_dims(input_image, 0))
+        input_image = tf.placeholder(tf.float32, shape=[FLAGS.image_size, FLAGS.image_size, 3], name='input_image')
+        cycle_gan.model()
+        if XtoY:
+            output_image = cycle_gan.G.sample(tf.expand_dims(input_image, 0))
+        else:
+            output_image = cycle_gan.F.sample(tf.expand_dims(input_image, 0))
 
-    output_image = tf.identity(output_image, name='output_image')
-    restore_saver = tf.train.Saver()
-    export_saver = tf.train.Saver()
+        output_image = tf.identity(output_image, name='output_image')
+        restore_saver = tf.train.Saver()
+        export_saver = tf.train.Saver()
 
-  with tf.Session(graph=graph) as sess:
-    sess.run(tf.global_variables_initializer())
-    latest_ckpt = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
-    restore_saver.restore(sess, latest_ckpt)
-    output_graph_def = tf.graph_util.convert_variables_to_constants(
-        sess, graph.as_graph_def(), [output_image.op.name])
+    with tf.Session(graph=graph) as sess:
+        sess.run(tf.global_variables_initializer())
+        latest_ckpt = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+        restore_saver.restore(sess, latest_ckpt)
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess, graph.as_graph_def(), [output_image.op.name])
 
-    tf.train.write_graph(output_graph_def, 'pretrained', model_name, as_text=False)
+        tf.train.write_graph(output_graph_def, 'pretrained', model_name, as_text=False)
+
 
 def main(unused_argv):
-  print('Export XtoY model...')
-  export_graph(FLAGS.XtoY_model, XtoY=True)
-  print('Export YtoX model...')
-  export_graph(FLAGS.YtoX_model, XtoY=False)
+    print('Export XtoY model...')
+    export_graph(FLAGS.XtoY_model, XtoY=True)
+    print('Export YtoX model...')
+    export_graph(FLAGS.YtoX_model, XtoY=False)
+
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
